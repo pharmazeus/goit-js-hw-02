34a4e9705585c10db074085930c24a6d3bf1d197
Pharma.zeus <markmaydeens@gmail.com>
2024-11-11 20:24:17 -0500

    4-tasks-js

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,11 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
-  const messageTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  return messageTransaction;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  let totalPrice = quantity * pricePerDroid;
+  return totalPrice <= customerCredits
+    ? `You ordered ${quantity} droids worth ${totalPrice} credits! `
+    : "Insufficient funds!";
 }
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
